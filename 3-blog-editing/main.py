ae26457798cafd88d7aa9af2ba5a66a8ca49a7d7
Lim En Xi <limenxi07@gmail.com>
2022-06-09 19:33:18 +0800

    basic func

--- 3-blog-editing/main.py
+++ 3-blog-editing/main.py
@@ -6,11 +6,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditor, CKEditorField
 
-
-## Delete this code:
-# import requests
-# posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
@@ -23,47 +18,49 @@ db = SQLAlchemy(app)
 
 ##CONFIGURE TABLE
 class BlogPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    subtitle = db.Column(db.String(250), nullable=False)
-    date = db.Column(db.String(250), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
-    img_url = db.Column(db.String(250), nullable=False)
-
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(250), unique=True, nullable=False)
+  subtitle = db.Column(db.String(250), nullable=False)
+  date = db.Column(db.String(250), nullable=False)
+  body = db.Column(db.Text, nullable=False)
+  author = db.Column(db.String(250), nullable=False)
+  img_url = db.Column(db.String(250), nullable=False)
+db.create_all()
+posts = BlogPost.query.all()
 
 ##WTForm
 class CreatePostForm(FlaskForm):
-    title = StringField("Blog Post Title", validators=[DataRequired()])
-    subtitle = StringField("Subtitle", validators=[DataRequired()])
-    author = StringField("Your Name", validators=[DataRequired()])
-    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
-    body = StringField("Blog Content", validators=[DataRequired()])
-    submit = SubmitField("Submit Post")
+  title = StringField("Blog Post Title", validators=[DataRequired()])
+  subtitle = StringField("Subtitle", validators=[DataRequired()])
+  author = StringField("Your Name", validators=[DataRequired()])
+  img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+  body = StringField("Blog Content", validators=[DataRequired()])
+  submit = SubmitField("Submit Post")
 
 
 @app.route('/')
 def get_all_posts():
-    return render_template("index.html", all_posts=posts)
+  return render_template("index.html", all_posts=posts)
 
 
 @app.route("/post/<int:index>")
 def show_post(index):
-    requested_post = None
-    for blog_post in posts:
-        if blog_post["id"] == index:
-            requested_post = blog_post
-    return render_template("post.html", post=requested_post)
+  requested_post = None
+  for blog_post in posts:
+    if blog_post["id"] == index:
+      requested_post = blog_post
+  return render_template("post.html", post=requested_post)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+  return render_template("about.html")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+  return render_template("contact.html")
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+  app.run(debug=True)
