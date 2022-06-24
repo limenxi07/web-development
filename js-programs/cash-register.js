d007c87f0c7a24ae2698182bf5c404bcde01837a
Lim En Xi <limenxi07@gmail.com>
2022-06-24 10:05:06 +0800

    func

--- js-programs/cash-register.js
+++ js-programs/cash-register.js
@@ -2,34 +2,35 @@ function checkCashRegister(price, cash, cid) {
   let change = cash - price;
   let returned = [];
   let cidCopy = JSON.parse(JSON.stringify(cid));
-  let i = 0
+  let i = 8
+  let multiplier = 0;
   const MONEY = {
-    'PENNY': 0.01,
-    'NICKEL': 0.05,
-    'DIME': 0.1,
-    'QUARTER': 0.25,
-    'ONE': 1,
-    'FIVE': 5,
-    'TEN': 10,
-    'TWENTY': 20,
     'ONE HUNDRED': 100,
+    'TWENTY': 20,
+    'TEN': 10,
+    'FIVE': 5,
+    'ONE': 1,
+    'QUARTER': 0.25,
+    'DIME': 0.1,
+    'NICKEL': 0.05,
+    'PENNY': 0.01,
   };
 
   for (var key in MONEY) {
     var value = MONEY[key];
-    let multiplier = 0;
+    multiplier = 0;
     for (change; change >= value && cid[i][1] > 0; change = (change - value).toFixed(2)) {
       multiplier++;
       returned[8-i] = [key, (value * multiplier)];
       cid[i][1] -= value;
     };
-    i++
+    i--
   };
 
   returned = returned.filter(x => x != null);
   let cidSum = 0;
-  for (let i = 0; i < cid.length; i++) {
-    cidSum += cid[i][1];
+  for (let j = 0; j < cid.length; j++) {
+    cidSum += cid[j][1];
   }
 
   if (change != 0) {
