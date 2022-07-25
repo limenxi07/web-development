162010bd0dcd304a27be190cb0684de4e01a5837
Lim En Xi <limenxi07@gmail.com>
2022-07-25 21:12:19 +0800

    file reorganisation

--- 2-js-algorithms/palindrome-checker.js
+++
@@ -1,6 +0,0 @@
-function palindrome(str) {
-  let formattedStr = str.toLowerCase().replace(/[^a-z0-9]+/gi, "");
-  let reversedStr = formattedStr.split("").reverse().join("");
-  return formattedStr == reversedStr
-}
-
